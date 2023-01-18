29dec37b0e78134fc00373d13aecdbe7791d02cf
unknown <piechnik10@gmail.com>
2023-01-18 22:04:11 +0100

    fix bugs

--- zestaw7/zadKol.py
+++ zestaw7/zadKol.py
@@ -1,21 +1,32 @@
 class Node:
     def __init__(self, val):
         self.val: int = val
-        self.next = None
+        self.next: Node = None
 
 
 def insert(head: Node, number):
-    start_node = head
-    while start_node is not None:
-        current_node = start_node
-        current_start_node = None
+    head_copy = head
+    loop2 = False
+    while True:
+        start_node = None
+        current_node = head_copy
         counter = 0
-        while current_node is not None:
-            if current_start_node is None and str(current_node)[-1] == str(number)[0]:
-                current_start_node = current_node
-            elif counter >= 2 and str(current_node)[0] == str(number)[-1]:
-                current_start_node.next = Node(number)
-                current_start_node.next.next = current_node
-                return counter
+        loop = False
+        while True:
+            if start_node is None and str(current_node.val)[-1] == str(number)[0]:
+                start_node = current_node
+            elif counter >= 2 and str(number)[-1] == str(current_node.val)[0]:
+                start_node.next = Node(number)
+                start_node.next.next = current_node
+                return counter - 1
+            else:
+                counter += 1
+            if loop and current_node == head_copy:
+                break
+            loop = True
             current_node = current_node.next
-        start_node = start_node.next
+        if loop2 and current_node == head_copy:
+            break
+        loop2 = True
+        head_copy = head_copy.next
+    return 0
